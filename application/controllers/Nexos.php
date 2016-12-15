e3bd0cdb17932749a247e0c891bb7fbb0f95b1a8
HGA <humden_58@hotmail.com>
2016-12-15 14:05:31 -0600

    funcion de deteccion de errores php borrada y agregando php.ini file para error log en nexosDigital/php-errors.log

--- application/controllers/Nexos.php
+++ application/controllers/Nexos.php
@@ -22,11 +22,6 @@ class Nexos extends CI_Controller {
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index($article='',$id=''){
-		
-		error_reporting(0); 
-        $old_error_handler = set_error_handler(&amp;quot;userErrorHandler&amp;quot;); 
-		
-		
 		if($article=='' && $id==''){
 			$this->load->view('inicio',
 						 	array(
@@ -48,41 +43,6 @@ class Nexos extends CI_Controller {
 						 );
 			
 			
-		}
-		
-						
-	}
-	
-	
-	
-
-     function userErrorHandler ($errno, $errmsg, $filename, $linenum, $vars) 
-     { 
-       $time=date(&amp;quot;d M Y H:i:s&amp;quot;);
-       // Get the error type from the error number 
-       $errortype = array (1 =&gt; &amp;quot;Error&amp;quot;, 
-                           2 =&gt; &amp;quot;Warning&amp;quot;, 
-                           4 =&gt; &amp;quot;Parsing Error&amp;quot;, 
-                           8 =&gt; &amp;quot;Notice&amp;quot;, 
-                           16 =&gt; &amp;quot;Core Error&amp;quot;, 
-                           32 =&gt; &amp;quot;Core Warning&amp;quot;, 
-                           64 =&gt; &amp;quot;Compile Error&amp;quot;, 
-                           128 =&gt; &amp;quot;Compile Warning&amp;quot;, 
-                           256 =&gt; &amp;quot;User Error&amp;quot;, 
-                           512 =&gt; &amp;quot;User Warning&amp;quot;, 
-                           1024 =&gt; &amp;quot;User Notice&amp;quot;); 
-       $errlevel=$errortype[$errno]; 
-
-       //Write error to log file (CSV format) 
-       $errfile=fopen(&amp;quot;errors.csv&amp;quot;,&amp;quot;a&amp;quot;); 
-       fputs($errfile,&amp;quot;&amp;quot;$time&amp;quot;,&amp;quot;$filename: 
-       $linenum&amp;quot;,&amp;quot;($errlevel) $errmsg&amp;quot;rn&amp;quot;);
-       fclose($errfile); 
-
-       if($errno!=2 &amp;amp;&amp;amp; $errno!=8) {
-          //Terminate script if fatal error 
-          die(&amp;quot;A fatal error has occurred. Script execution has been aborted&amp;quot;); 
-       } 
-     }
-	
+		}			
+	}	
 }

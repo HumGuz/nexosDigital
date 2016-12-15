92fd718c137155fa85a88473e93501ff26f2a2a4
HGA <humden_58@hotmail.com>
2016-12-15 14:24:02 -0600

    nueva function para detectar erroes de php

--- application/controllers/Nexos.php
+++ application/controllers/Nexos.php
@@ -5,6 +5,8 @@ class Nexos extends CI_Controller {
 	public function __construct(){
     	parent::__construct();
 		$this->load->model('articles_model');
+		 error_reporting(0); 
+   		$old_error_handler = set_error_handler("userErrorHandler");
     }
 	/**
 	 * Index Page for this controller.
@@ -45,4 +47,39 @@ class Nexos extends CI_Controller {
 			
 		}			
 	}	
+
+
+	
+
+   function userErrorHandler ($errno, $errmsg, $filename, $linenum,  $vars) 
+   {
+     $time=date("d M Y H:i:s"); 
+     // Get the error type from the error number 
+     $errortype = array (1    => "Error",
+                         2    => "Warning",
+                         4    => "Parsing Error",
+                         8    => "Notice",
+                         16   => "Core Error",
+                         32   => "Core Warning",
+                         64   => "Compile Error",
+                         128  => "Compile Warning",
+                         256  => "User Error",
+                         512  => "User Warning",
+                         1024 => "User Notice");
+      $errlevel=$errortype[$errno];
+
+      //Write error to log file (CSV format) 
+      $errfile=fopen("errors.csv","a"); 
+      fputs($errfile,"\"$time\",\"$filename: 
+      $linenum\",\"($errlevel) $errmsg\"\r\n"); 
+      fclose($errfile);
+
+      if($errno!=2 && $errno!=8) {
+         //Terminate script if fatal error
+         die("A fatal error has occurred. Script execution has been aborted");
+      } 
+   }
+
+
+
 }
